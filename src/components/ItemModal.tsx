81c38247326bfb252779ca37dffe853e1efbaed0
Rishon Dass <rishondass@gmail.com>
2024-11-18 22:07:16 -0600

    started to implement image feature to items

--- src/components/ItemModal.tsx
+++ src/components/ItemModal.tsx
@@ -3,7 +3,7 @@ import { IoIosClose } from "react-icons/io";
 import { FaCamera } from "react-icons/fa";
 import {useState} from "react";
 import Image from "next/image";
-
+import Camera from "@/components/Camera";
 import { v4 as uuid } from 'uuid';
 import { useItems } from '@/lib/globalStates';
 type Props = {
@@ -20,42 +20,43 @@ type Props = {
 const ItemModal = ({krateID, id,name, description, quantity, image, toggle,handleEdit}:Props) => {
   const items = useItems(state=>state.items);
   const setItems = useItems(state=>state.setItems);
-  const [dataUri, setDataUri] = useState("")
+  const [imageUri, setImageUri] = useState<string | ImageData | undefined>();
   const [toggleCamera, setToggleCamera] = useState(false);
-  const [itemInfo, setItemInfo] = useState<itemType>({name:name||"",description:description||"",quantity:quantity||1,image:image||"",id: id||uuid(),krateID: krateID || ""});
+  const [itemInfo, setItemInfo] = useState<itemType>({name:name||"",description:description||"",quantity:quantity||1,image:image||(uuid()+".png"),id: id||uuid(),krateID: krateID || ""});
 
   const cameraToggle = ()=>{
     setToggleCamera(!toggleCamera);
   }
 
-  
-
-  function handleTakePhotoAnimationDone (dataUri:string) {
-    console.log('takePhoto', dataUri);
-    setDataUri(dataUri);
-    cameraToggle();
+  const setImageProp = (imageData:string | ImageData | undefined)=>{
+    setImageUri(imageData);
   }
-
+  //TODO: add api changes for images
   async function addItem(){
     const add = await fetch('/api/v1/krate/items/'+krateID,{
       method: "POST",
-      body: JSON.stringify(itemInfo),
+      body: JSON.stringify({...itemInfo,imageUri: imageUri }),
       headers: {
         'Content-Type': 'application/json',
       }
     });
     if(add.status === 200){
       const temp = [...items];
-      temp.push(itemInfo);
+      if(imageUri){
+        temp.push({...itemInfo});
+      }else{
+        temp.push({...itemInfo, image: ""});
+      }
       setItems(temp);
       toggle?.();
     }
   }
 
   async function saveItem(){
+    const newImageURL = uuid()+ ".png";
     const save = await fetch("/api/v1/item/"+id,{
       method: "PUT",
-      body: JSON.stringify(itemInfo),
+      body: JSON.stringify({...itemInfo, newImage: newImageURL, imageUri: imageUri}),
       headers: {
         'Content-Type': 'application/json',
       }
@@ -63,7 +64,11 @@ const ItemModal = ({krateID, id,name, description, quantity, image, toggle,handl
     if(save.status === 200){
       const index = items.findIndex(item=>item.id === id);
       const temp = [...items];
-      temp[index] = itemInfo;
+      if(imageUri){
+        temp[index] = {...itemInfo, image: newImageURL};
+      }else{
+        temp[index] = itemInfo;
+      }
       setItems(temp);
       handleEdit?.("","",-1,"","");
     }
@@ -72,9 +77,13 @@ const ItemModal = ({krateID, id,name, description, quantity, image, toggle,handl
 
 
   return <>
-    <div className='z-20 absolute left-0 top-0'>
-      {/* {toggleCamera&&<Camera isFullscreen={true} idealFacingMode={FACING_MODES.ENVIRONMENT} isMaxResolution={true} onTakePhotoAnimationDone={handleTakePhotoAnimationDone}/>} */}
-    </div>
+    {
+      toggleCamera&&
+      <div className='z-20 absolute left-0 top-0 h-screen w-screen'>
+        <Camera toggle={cameraToggle} setImage={setImageProp}/>
+      </div>
+    }
+    
     <div className='z-10 absolute left-0 top-0 bg-black/60 w-full h-full'>
       <div className='flex justify-center items-center h-full'>
         <div className='bg-white rounded-md h-4/5 w-11/12 p-2'>
@@ -99,9 +108,9 @@ const ItemModal = ({krateID, id,name, description, quantity, image, toggle,handl
               <label htmlFor="location">Description:</label>
               <textarea name="" id="" defaultValue={description} className='outline-none bg-gray-200 rounded-md h-36 p-2' onChange={(e)=>{setItemInfo({...itemInfo,description:e.target.value})}}></textarea>
             </div>
-            {dataUri?
+            {imageUri?
               <div className='relative h-32 col-span-2 rounded-md cursor-pointer' onClick={cameraToggle}>
-                <Image src={dataUri} alt={"krate image"} fill={true} className='object-contain'/>
+                <Image src={imageUri as string} alt={"krate image"} fill={true} className='object-contain'/>
               </div>
             :
               <div className="bg-gray-300 rounded-md h-32 flex flex-col justify-center items-center col-span-2 cursor-pointer" onClick={cameraToggle}>
